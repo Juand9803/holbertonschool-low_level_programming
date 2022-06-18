870959c96195563039ba27f7fd1c0bf3a04be46c
Juand9803 <5260@holbertonschool.com>
2022-06-18 05:32:46 -0700

    version 29.9

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - seven
+ *
+ *Return: 0
+ */
+
+inte main(void)
+{
+	char la;
+
+	for (la = 'z' ; la >= 'a' ; la--)
+	{
+		putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
