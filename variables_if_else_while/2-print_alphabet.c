4887bfef0eb20362560a21da51120d4c702f45c5
Juand9803 <5260@holbertonschool.com>
2022-06-18 04:50:44 -0700

    version 29

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ *main - two
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char le;
+
+	for (le = 'a' ; le <= 'z' ; le++)
+	{
+			putchar(le);
+	}
+	putchar('\n');
+	return (0);
+}
