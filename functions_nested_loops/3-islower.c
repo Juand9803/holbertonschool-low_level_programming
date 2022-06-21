7a84efb424134fab273684f84a554c57caa41c7b
Juand9803 <5260@holbertonschool.com>
2022-06-21 15:31:32 -0700

    version 31.8

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_islower - cdb
+ *
+ *Return: 1 id c is lowercase, 0 otherwise
+ */
+int _slower(int c)
+{
+if (c >= 97 && c <= 122)
+	return (1);
+else
+	return (0);
+}
