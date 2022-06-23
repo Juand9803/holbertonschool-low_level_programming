631a632f083ec92de3b6973d5fd3e347bb1b675e
Juand9803 <5260@holbertonschool.com>
2022-06-23 04:44:42 -0700

    version 33.4

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - eleven
+ *@n: number diferent
+ *
+ */
+void print_to_98(int n)
+{
+while (n != 98)
+	if (n < 98)
+	{
+		printf("%i", n);
+		if (n != 98)
+		{
+			n--;
+			printf(",");
+			printf(" ");
+		}
+	}
+printf("%i", n);
+printf("\n");
+}
