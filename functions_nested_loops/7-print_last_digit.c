e7f9422933f3ddf4578f9b159402172f90d28047
Juand9803 <5260@holbertonschool.com>
2022-06-22 18:31:35 -0700

    version 32.1

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_last_digit - print
+ *@n: last digit
+ *Return: Last digit
+ */
+int print_last_digit(int n)
+{
+int l = 0, x = 0;
+l = n % 10;
+if (l < 0)
+	l = l * (-1);
+x = '0' + l;
+_putchar(x);
+return (l);
+}
